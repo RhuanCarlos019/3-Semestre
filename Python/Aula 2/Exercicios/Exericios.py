67c04f20a1c09392b2e060b4874f04aa2dbec0a1
Rhuan Carlos <123770612+RhuanCarlos019@users.noreply.github.com>
2024-03-05 08:53:36 -0300

    Atualização

--- Python/Aula 2/Exercicios/Exericios.py
+++ Python/Aula 2/Exercicios/Exericios.py
@@ -1,4 +1,4 @@
-#Exericio 1
+#Exericio 1 :
 
 a = 5
 b = 6
@@ -19,7 +19,7 @@ area = pi * (raio ** 2)
 print("Área do círculo:", area)
 
 
-# Exercicio 2
+# Exercicio 2 :
 
 nome =  "Federico"
 sobrenome = "Valverde"
@@ -27,7 +27,7 @@ nome_completo = nome + " " + sobrenome
 print("Nome completo "+ nome_completo)
 
 
-#Declarando a frase 
+#Declarando a frase :
 frase = "O Santos é o maior time do mundo"
 
 #Convertendo para letra maiúscula
@@ -39,24 +39,24 @@ frase_minuscula = frase.lower()
 print("Frase em minúsculo:"+ frase_minuscula)
 
 
-#Exercicio 3
+#Exercicio 3 :
 
 cores = ["verde", "azul", "amarelo", "laranja"]
 print("Lista de cores inicial: " +cores)
 
-#Adicionando outras cores a lista
+#Adicionando outras cores a lista :
 cores.append = "preto"
 cores.append = "Roxo"
 
-# Declarando uma tupla com as coordenadas (latitude, longitude) de um local
+# Declarando uma tupla com as coordenadas (latitude, longitude) de um local :
 coordenadas = (40.7128, -74.0060)
 
-# Imprimindo cada valor separadamente
+# Imprimindo cada valor separadamente :
 print("Latitude:", coordenadas[0])
 print("Longitude:", coordenadas[1])
 
 
-#Exercicio 4
+#Exercicio 4 :
 
 tem_sol = True
 tem_chuva = False
@@ -67,7 +67,7 @@ else:
     print("Não é um dia agradável")
 
 
-# Verificação dos nº pares
+# Verificação dos nº pares :
 numero1 = int(input("Digite o primeiro número: "))
 numero2 = int(input("Digite o segundo número: "))
 
@@ -76,14 +76,14 @@ if numero1 % 2 == 0 and numero2 % 2 == 0:
 else:
     print("Pelo menos um dos números não é par.")
 
-# Múltiplos de 3 e ímpares em uma lista
+# Múltiplos de 3 e ímpares em uma lista :
 lista_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 for numero in lista_numeros:
     if numero % 3 == 0 and numero % 2 != 0:
         print(numero, "é múltiplo de 3 e ímpar.")
 
-# Verificação da idade
+# Verificação da idade :
 idade = int(input("Digite sua idade: "))
 
 if idade >= 18 and idade <= 65:
